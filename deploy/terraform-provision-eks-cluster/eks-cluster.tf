529a9a7dac564cf99c99eb03c3273c3ea06026ce
root <root@ip-172-31-26-51.ap-southeast-1.compute.internal>
2021-04-14 14:43:38 +0000

    cleanen some terraform code

--- deploy/terraform-provision-eks-cluster/eks-cluster.tf
+++ deploy/terraform-provision-eks-cluster/eks-cluster.tf
@@ -4,11 +4,6 @@ module "eks" {
   cluster_version = "1.18"
   subnets         = module.vpc.private_subnets
 
-  tags = {
-    Environment = "training"
-    GithubRepo  = "terraform-aws-eks"
-    GithubOrg   = "terraform-aws-modules"
-  }
 
   vpc_id = module.vpc.vpc_id
 
--- deploy/terraform-provision-eks-cluster/versions.tf
+++ deploy/terraform-provision-eks-cluster/versions.tf
@@ -5,26 +5,6 @@ terraform {
       version = ">= 3.20.0"
     }
 
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
-    }
-
-    local = {
-      source  = "hashicorp/local"
-      version = "2.0.0"
-    }
-
-    null = {
-      source  = "hashicorp/null"
-      version = "3.0.0"
-    }
-
-    template = {
-      source  = "hashicorp/template"
-      version = "2.2.0"
-    }
-
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.1"
